eb70bd704e4cdb22fff8d636e4e404453172adef
Nini Chang <ninichang7066@gmail.com>
2022-11-29 10:20:04 +0800

    Rename file and add endpoint for filter api

--- backend/routes/api/filter.js
+++ backend/routes/api/filter.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 var Item = mongoose.model("Item");
 var User = mongoose.model('User');
 
-router.get('/', function(req, res, next) {
+router.get('/title', function(req, res, next) {
   let title = '';
   if (typeof req.title !== 'undefined') {
     title = req.title;
