ce8ad64d7bff61ff8a4b24003cae2884b6e3f861
Nini Chang <ninichang7066@gmail.com>
2022-11-29 10:12:58 +0800

    (Search Party) Add title.js for filtering

---
+++ backend/routes/api/title.js
@@ -0,0 +1,21 @@
+var router = require("express").Router();
+var mongoose = require("mongoose");
+var Item = mongoose.model("Item");
+var User = mongoose.model('User');
+
+router.get('/', function(req, res, next) {
+  let title = '';
+  if (typeof req.title !== 'undefined') {
+    title = req.title;
+  }
+
+  Promise.resolve(title)
+    .then(function(item) {
+      return res.json({
+        projectList: req.item
+      });
+    })
+    .catch(next)
+});
+
+module.exports = router;
